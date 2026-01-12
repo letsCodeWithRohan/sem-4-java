185b69d5b9a6dc6330601c5a11caf2004373b849
Rohan Chaudhari <chaudharirohan2825@gmail.com>
2026-01-12 11:12:52 +0530

    Modified division.java

--- Set-6/Division.java
+++ Set-6/Division.java
@@ -1,5 +1,13 @@
-class One {
+/* 6.1 Take the value of denominator and numerator from user using command-line argument.
+Implement the concept of exception handling to manage all possible run-time error.
+ */
+
+class Division {
     public static void main(String[] args){
+        if(args.length != 2){
+            System.out.println("Usage : java Division <numerator> <denominator>");
+            return;
+        }
         try{
             int result = Integer.parseInt(args[0]) / Integer.parseInt(args[1]);
             System.out.println("Answer is : " + result);
@@ -7,6 +15,8 @@ class One {
             System.out.println("Invalid Input Format : " + e.getMessage());
         } catch(ArithmeticException e){
             System.out.println("Invalid Operation : " + e.getMessage());
+        } catch(Exception e){
+            System.out.println("An Unexpected Error : " + e.getMessage());
         } 
     }
 }
